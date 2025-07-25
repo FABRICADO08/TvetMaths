d3c4ecfc4e440cbf617c1c54746c85d2b5a51810
FABRICADO <93220522+FABRICADO08@users.noreply.github.com>
2025-07-25 12:22:32 +0200

    Create MathSolverApplication.java

---
+++ math-solver/src/main/java/com/tvet/mathsolver/MathSolverApplication.java
@@ -0,0 +1,13 @@
+package com.tvet.mathsolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathSolverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathSolverApplication.class, args);
+    }
+
+}
